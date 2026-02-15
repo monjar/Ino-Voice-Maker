d6b41a45276bfbe80df536d56fd50bd2af1e0e7d
amirali <amirali.mnj@gmail.com>
2026-02-15 10:23:00 +0330

    init: scaffold vite + react project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif', color: '#fff', background: '#111', minHeight: '100vh' }}>
+      <h1>Ino Voice Maker</h1>
+      <p>Robot SFX designer â€” coming soon.</p>
+    </div>
+  )
+}
+
+export default App
